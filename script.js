2756b17e2bd1ac02845907d2586ae2256adccdcd
malikhussain26 <hussain.am2697@gmail.com>
2024-10-04 00:35:24 -0500

    event listeners

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log('rock, paper, scissors')
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
+
+const resultSection = document.getElementById('result');
+
+rockButton.addEventListener('click', () => playRound('rock'));
+paperButton.addEventListener('click', () => playRound('paper'));
+scissorsButton.addEventListener('click', () => playRound('scissors'));
