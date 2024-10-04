fb560d7607d55327947e299c6b13d21073b14eec
malikhussain26 <hussain.am2697@gmail.com>
2024-10-04 00:30:00 -0500

    declaring variables in js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log('rock, paper, scissors')
+
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
