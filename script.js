5428be22c441f175e47d64963eb9e5babf0d93c5
malikhussain26 <hussain.am2697@gmail.com>
2024-10-04 22:23:02 -0500

    added some attribute modification

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const winsElement = document.getElementById('wins');
 const lossesElement = document.getElementById('losses');
 const tiesElement = document.getElementById('ties');
 
+let roundCount = 0;
+const maxRounds = 3;
 
 gameForm.addEventListener('submit', (event) => {
     const playerChoiceInput = document.querySelector('input[name="choice"]:checked');
@@ -24,6 +26,22 @@ gameForm.addEventListener('submit', (event) => {
     const result = determineWinner(playerChoice, computerChoice);
     updateResult(result);
     updateScore(result);
+
+    roundCount++;
+
+    if (roundCount >= maxRounds) {
+        //Determine winner based on the scores
+        const wins = parseInt(winsElement.textContent);
+        const losses = parseInt(lossesElement.textContent);
+        if (wins > losses) {
+            updateResult('You won best of 3!');
+        } else if (losses > wins) {
+            updateResult('You lost best of 3!');
+        } else {
+            updateResult("It'a a tie!");
+        }
+        disableButtons();
+    }
 });
 
 function getComputerChoice() {
