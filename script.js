ea4b4f0a1ea84583eec3e6112fe568458bb93f1f
malikhussain26 <hussain.am2697@gmail.com>
2024-10-04 23:19:15 -0500

    updates

--- script.js
+++ script.js
@@ -46,7 +46,6 @@ gameForm.addEventListener('submit', (event) => {
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];
     const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-
     window.alert("The computer Chose: " + computerChoice);
     return computerChoice;
 
@@ -59,25 +58,27 @@ function determineWinner(playerChoice, computerChoice) {
         (playerChoice === "rock" && computerChoice === "scissors") ||
         (playerChoice === "paper" && computerChoice === "rock") ||
         (playerChoice === "scissors" && computerChoice === "paper") ) {
-            return "You win!";z
+            return "You win!";
         } else {
-            return "You lose!";
+            return "You lose!r";
         }
 }
 
 function updateResult(result) {
     resultSection.textContent = result;
-    if (result === 'Win') {
-        window.alert("Congratulations! You Won!")
-    } else if (result === 'Lose') {
-        window.alert("Sorry, you lost. Try again!");
+    if (result === 'You Win') {
+        resultSection.style.color = 'green';
+        resultSection.innerHTML = `<h2>Congrats, You Won!</h2><img src="https://media0.giphy.com/media/uRAhwxlVBP6ied6EgB/giphy.gif?cid=6c09b9528ta6gwv0w3asahszz3ur4kuh8k8c8ik0c4jsn1gw&ep=v1_internal_gif_by_id&rid=giphy.gif&ct=g">`;
+    } else if (result === 'You Lose') {
+        resultSection.style.color = 'red'
+        resultSection.innerHTML = `<h2>Sorry, You Lost. Play again!</h2><img src="https://media.baamboozle.com/uploads/images/142775/1633674782_2028546_url.gif">`;
     }
 }
 
 function updateScore(result) {
-    if (result === 'Win') {
+    if (result === 'You Win!') {
         winsElement.textContent = parseInt(winsElement.textContent) + 1;
-    } else if (result === 'Lose') {
+    } else if (result === 'You Lose!') {
         lossesElement.textContent = parseInt(lossesElement.textContent) + 1;
     } else {
         tiesElement.textContent = parseInt(tiesElement.textContent) + 1;
@@ -88,7 +89,7 @@ function disableButtons() {
     const rockButton = document.getElementById('rock');
     const paperButton = document.getElementById('paper');
     const scissorsButton = document.getElementById('scissors');
-    
+
     rockButton.setAttribute('disabled', true);
     paperButton.setAttribute('disabled', true);
     scissorsButton.setAttribute('disabled', true);
